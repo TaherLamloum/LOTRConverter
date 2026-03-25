dc53a250f811dc371508860e9f2295463cd15818
TaherLamloum <taherelnehros@gmail.com>
2026-03-26 02:06:09 +0300

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by taher lamloum on 26/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
